3995a493f56f00bb6e94b324f512967d071b5fe5
Faruk <f.hodzic.inf24@stud.bbbaden.ch>
2026-08-20 23:52:40 +0200

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ProteinTracker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSaveGoalClicked(object? sender, EventArgs e)
+        {
+            GoalLabel.Text = "Dein Tagesziel: " + ProteinGoalEntry.Text + " g Protein";
+        }
+    }
+}
