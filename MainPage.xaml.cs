f65c25f6a39fc2a26fe3c0749203909c384cec13
Faruk <f.hodzic.inf24@stud.bbbaden.ch>
2026-08-21 11:33:03 +0200

    Add files via upload

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,6 +2,10 @@
 {
     public partial class MainPage : ContentPage
     {
+
+        double proteinGoal = 0;
+        double totalProtein = 0;
+
         public MainPage()
         {
             InitializeComponent();
@@ -9,7 +13,27 @@
 
         private void OnSaveGoalClicked(object? sender, EventArgs e)
         {
-            GoalLabel.Text = "Dein Tagesziel: " + ProteinGoalEntry.Text + " g Protein";
+            proteinGoal = double.Parse(ProteinGoalEntry.Text);
+
+            GoalLabel.Text = "Dein Tagesziel: " + proteinGoal + " g Protein";
+        }
+
+        private void OnAddFoodClicked(object? sender, EventArgs e)
+        {
+            double protein = double.Parse(ProteinAmountEntry.Text);
+
+            totalProtein = totalProtein + protein;
+
+            TotalProteinLabel.Text =
+                "Aktuell: " + totalProtein + " / " + proteinGoal + " g Protein";
+
+            Label foodLabel = new Label();
+
+            foodLabel.Text =
+                FoodNameEntry.Text + " - " + ProteinAmountEntry.Text + " g Protein";
+
+            FoodList.Children.Add(foodLabel);
         }
     }
 }
+
